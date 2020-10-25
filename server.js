9cdfa3a6a192659b40cdc82efdc6961ee659b3d8
Zahra Azin <ztara.azin@gmail.com>
2020-10-24 22:10:59 -0400

    add server file

---
+++ server.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+
+const express = require("express");
+
+const mongoose =require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost:27017/googlebooks",
+  { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true }
+);
+
+
+app.listen(PORT, function() {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
