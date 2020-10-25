9a9000baf1e6d34a7c3cde9abcd47b5557c8c9fa
Zahra Azin <ztara.azin@gmail.com>
2020-10-24 22:18:24 -0400

    updating server.js

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const mongoose =require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const path = require('path');
 
 
 app.use(express.urlencoded({ extended: true }));
