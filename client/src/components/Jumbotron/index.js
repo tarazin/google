0303692f0c51b073e484ecf64eccc90057b6be5f
Zahra Azin <ztara.azin@gmail.com>
2020-10-24 23:37:43 -0400

    update index file

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -4,8 +4,8 @@ function Jumbotron() {
   return(
     <div class="container-fluid">
       <div class="jumbotron text-center">
-        <h1 class="display-4">Whoomp! There It Is 📚</h1>
-        <p class="lead">Look for Books and then Save them for Later😏</p>
+        <h1 class="display-4">Yoo-Hoo! There It Is 📚</h1>
+        <p class="lead">Look for Books and Save them!</p>
       </div>
     </div>
   )
